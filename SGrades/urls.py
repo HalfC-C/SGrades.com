51a7cbdddf7654cd0fabfca4548d87869427c14f
SaraSerrano7 <sasecu@gmail.com>
2022-03-27 13:12:49 +0200

    editing option almost done

--- SGrades/urls.py
+++ SGrades/urls.py
@@ -12,19 +12,24 @@ urlpatterns = [
     path('editormode/', EditorModeView, name='editormode'),
     
     path('editormode/editornewstudent/', EditorNewStudentView.as_view(), name='editornewstudent'),
+    path('editormode/student_list/', StudentListView, name='student_list'),
     path('student/<str:pk>/', StudentDetailView.as_view(), name='student_detail'),
     
     path('editormode/editornewgrade/', EditorNewGradeView.as_view(), name='editornewgrade'),
+    path('grade/<int:pk>/edit_change_grade/', EditorChangeGradeView.as_view(), name='edit_change_grade'),
     path('editormode/grades_list/', GradesListView, name='grades_list'),
     path('grade/<int:pk>', GradeDetailView.as_view(), name='grade_detail'),
     
     path('editormode/editornewitem/', EditorNewItemView.as_view(), name='editornewitem'),
+    path('editormode/item_list/', ItemListView, name='item_list'),
     path('item/<str:pk>/', ItemDetailView.as_view(), name='item_detail'),
     
     path('editormode/editornewsubject/', EditorNewSubjectView.as_view(), name='editornewsubject'),
+    path('editormode/subject_list/', SubjectListView, name='subject_list'),
     path('subject/<str:pk>/', SubjectDetailView.as_view(), name='subject_detail'),
     
     path('editormode/editornewsubmit/', EditorNewSubmitView.as_view(), name='editornewsubmit'),
+    path('editormode/submit_list/', SubmitListView, name='submit_list'),
     path('submit/<int:pk>/', SubmitDetailView.as_view(), name='submit_detail'),
 
 ]
--- SGrades/views.py
+++ SGrades/views.py
@@ -225,9 +225,20 @@ class EditorNewStudentView(CreateView):
     template_name = 'edit_new_student.html'    
     fields = ['Student_Nif', 'Name','Surname', 'Course']
 
+class EditorChangeStudentView(UpdateView):
+    model = Student
+    template_name = 'edit_change_student.html'
+    fields = ['Student_Nif', 'Name','Surname', 'Course']
+
+def StudentListView(request):
+    return render(request, 'student_list.html', {'Student_list' : Student.objects.all()})
+
 class StudentDetailView(DetailView):
     model = Student
     template_name = 'detail_student.html'
+
+
+
 
 class EditorNewGradeView(CreateView):
     model = Grade#, Subject, Grade, Submit, Item
@@ -246,29 +257,55 @@ class GradeDetailView(DetailView):
     model = Grade
     template_name = 'detail_grade.html'
 
+
+
 class EditorNewSubmitView(CreateView):
     model = Submit#, Subject, Grade, Submit, Item
     template_name = 'edit_new_submit.html'    
     fields = ['Student', 'Item_Submitted','Punctuation']
 
+class EditorChangeSubmitView(UpdateView):
+    model = Submit
+    template_name = 'edit_change_submit.html'
+    fields = ['Student', 'Item_Submitted','Punctuation']
+
+def SubmitListView(request):
+    return render(request, 'submit_list.html', {'Submit_list' : Submit.objects.all()})
 class SubmitDetailView(DetailView):
     model = Submit
     template_name = 'detail_submit.html'
+
+
 
 class EditorNewItemView(CreateView):
     model = Item#, Subject, Grade, Submit, Item
     template_name = 'edit_new_item.html'    
     fields = ['Item_Name', 'Item_From_Subject','Ponderation', 'Date']
 
+class EditorChangeItemView(UpdateView):
+    model = Item
+    template_name = 'edit_change_item.html'
+    fields = ['Item_Name', 'Item_From_Subject','Ponderation', 'Date']
+
+def ItemListView(request):
+    return render(request, 'item_list.html', {'Item_list' : Item.objects.all()})
 class ItemDetailView(DetailView):
     model = Item
     template_name = 'detail_item.html'
+
+
 
 class EditorNewSubjectView(CreateView):
     model = Subject#, Subject, Grade, Submit, Item
     template_name = 'edit_new_subject.html'    
     fields = ['Subject_Name', 'Course']
 
+class EditorChangeSubjectView(UpdateView):
+    model = Subject
+    template_name = 'edit_change_subject.html'
+
+def SubjectListView(request):
+    return render(request, 'subject_list.html', {'Subject_list' : Subject.objects.all()})
 class SubjectDetailView(DetailView):
     model = Subject
     template_name = 'detail_subject.html'
